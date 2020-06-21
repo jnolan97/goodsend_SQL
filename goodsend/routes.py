ba56e6c3e6bd313586050ef64e6760e796d3e993
jnolan97 <johnmnolan97@gmail.com>
2020-06-20 20:48:36 -0500

    Sendgrid now sends 2 emails, and users can update info

--- goodsend/routes.py
+++ goodsend/routes.py
@@ -2,7 +2,7 @@ from goodsend import app, db, Message, mail
 from flask import render_template, request, redirect, url_for
 from goodsend.forms import UserInfoForm, LoginForm
 #import models
-from goodsend.models import Waitlist, Onboarded, check_password_hash
+from goodsend.models import Waitlist, Onboarded, check_password_hash, generate_password_hash
 from flask_login import login_required,login_user,current_user,logout_user
 import os
 import stripe
@@ -47,7 +47,7 @@ def register():
         # Save info into database
         db.session.commit()
         #Email service funnel for new users
-        msg = Message(f'{email} has signed up!', recipients=[email])
+        msg = Message(f'{email} has signed up!', recipients=[email, 'goodsendtest1@gmail.com'])
         msg.body =('Another user has signed up')
         msg.html = ('<h1> Welcome to Goodsend! </h1>' '<p> Thank you for signing up! </p>')
         mail.send(msg)
@@ -82,4 +82,26 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
+@app.route('/update/<int:user_id>',methods = ['GET','POST'])
+@login_required
+def update(user_id):
+    update = Waitlist.query.get_or_404(user_id)
+    update_form = UserInfoForm()
+
+    if request.method == 'POST' and update_form.validate():
+        first_name = update_form.first_name.data
+        last_name = update_form.last_name.data
+        email = update_form.email.data
+        phone_number = update_form.phone_number.data
+        password = update_form.password.data
+        user_id = current_user.id
+        update.first_name = first_name
+        update.last_name = last_name
+        update.email = email
+        update.phone_number = phone_number
+        update.password = generate_password_hash(password)
+        update.user_id = user_id
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('update.html',update_form = update_form)
 
